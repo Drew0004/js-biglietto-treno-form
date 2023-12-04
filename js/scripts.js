8fb9bae6e801f4fcd94ed3f5f125c14ca457cec1
Drew0004 <drdrewgraphic@gmail.com>
2023-12-04 17:35:28 +0100

    Terminate stampe  in HTML-JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ MILESTONE 1:
 Iniziamo implementando il programma senza alcuna estetica: 
 usando esclusivamente due input e un bottone (non stilizzati), 
 realizziamo le specifiche scritte sopra. La risposta finale (o output)
- sarà da scrivere in console.
+ sarà da scrivere in console.                                                  (OK)
 MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e 
 funzionante allora realizzeremo un form in pagina in cui l'utente 
@@ -21,7 +21,10 @@ Il recap dei dati e l'output del prezzo finale, andranno quindi stampati
 in pagina (il prezzo dovrà essere formattato con massimo due decimali, per 
 indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 Nota:
-Se non vi sentite particolarmente creativi, questa potrebbe essere un'implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
+Se non vi sentite particolarmente creativi, questa 
+potrebbe essere un'implementazione da seguire per il secondo milestone.
+Potete scegliere di implementare una soluzione completamente diversa oppure
+ simile, ma in ogni caso cercate di farla vostra.
 */
 
 const userName = document.getElementById('user-name'); 
@@ -35,6 +38,7 @@ const myButton = document.getElementById('special-button');
 myButton.addEventListener('click', function(){
     const nameInput = (userName.value);
     console.log("Il nome dell'utente è " + nameInput);
+    document.getElementById('user-result-name').innerHTML = nameInput;
     
     const kmInput = parseInt(userKm.value);
     console.log("L'utente ha inserito: " + kmInput + ' Km');
@@ -45,16 +49,27 @@ myButton.addEventListener('click', function(){
 
     let price = (kmInput * 0.21);
     console.log("L'utente paga: " + price + '€');
+    document.getElementById('price-results').innerHTML = `${price}€`;
 
     let age = document.getElementById('user-age').value;
 
     console.log('age: ', age);
 
     if (age < 18){
+        let priceDiscounted20 = (price * 0.8);
         console.log("Il prezzo in sconto è: " + price * 0.8 + '€');
+        document.getElementById('price-results').innerHTML = `${priceDiscounted20}€`;
     }
     else if (age >= 65){
+        let priceDiscounted40 = (price * 0.6);
         console.log("Il prezzo in sconto è: " + price * 0.6 + '€');
+        document.getElementById('price-results').innerHTML = `${priceDiscounted40}€`;
     }
+
+    const randomMathsmall = Math.floor(Math.random()*10) + 1;
+    document.getElementById('r-math-generator-small').innerHTML = randomMathsmall;
+
+    const randomMathBig = Math.floor(Math.random()*90000) + 10000;
+    document.getElementById('r-math-generator').innerHTML = randomMathBig;
 });
 
